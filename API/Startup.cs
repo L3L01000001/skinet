3c9f5ce14d72bf8511a3b8f2585ff419c409161f
L3L01000001 <49353673+L3L01000001@users.noreply.github.com>
2021-05-25 18:49:51 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Core.Entities;
+using Core.Interfaces;
 
 namespace API
 {
@@ -29,7 +31,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddSwaggerGen(); 
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
